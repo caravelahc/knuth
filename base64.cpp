3f41c752734c24808c28959ced2eca3d5b8cd5c7
Ranieri Althoff <ranisalt@gmail.com>
2018-05-29 21:33:20 -0300

    Fix base64 encode behavior with padding

--- base64.cpp
+++ base64.cpp
@@ -17,15 +17,20 @@ std::string encode(const std::string& s) {
     auto curr = std::begin(s);
 
     auto remaining = s.size();
-    while (remaining > 0) {
+    while (remaining >= 3) {
         unsigned buffer = (*curr << 16u) + (*(curr + 1) << 8u) + *(curr + 2);
-        switch (remaining % 4) {
-        case 3:
-            output.push_back(alphabet[(buffer >> 18u) % 64]);
-            output.push_back(alphabet[(buffer >> 12u) % 64]);
-            output.push_back(alphabet[(buffer >> 6u) % 64]);
-            output.push_back(alphabet[(buffer >> 0u) % 64]);
-            break;
+
+        output.push_back(alphabet[(buffer >> 18u) % 64]);
+        output.push_back(alphabet[(buffer >> 12u) % 64]);
+        output.push_back(alphabet[(buffer >> 6u) % 64]);
+        output.push_back(alphabet[(buffer >> 0u) % 64]);
+
+        remaining -= 3;
+        std::advance(curr, 3);
+    }
+
+    unsigned buffer = (*curr << 16u) + (*(curr + 1) << 8u) + *(curr + 2);
+    switch (remaining) {
         case 2:
             output.push_back(alphabet[(buffer >> 18u) % 64]);
             output.push_back(alphabet[(buffer >> 12u) % 64]);
@@ -38,9 +43,6 @@ std::string encode(const std::string& s) {
             output.push_back('=');
             output.push_back('=');
             break;
-        }
-
-        remaining -= 3;
     }
     return output;
 }
@@ -55,9 +57,7 @@ int main() {
     assert(knuth::base64::encode("M") == "TQ=="s);
     assert(knuth::base64::encode("Ma") == "TWE="s);
     assert(knuth::base64::encode("Man") == "TWFu"s);
-    assert(knuth::base64::decode("TQ==") == "M"s);
-    assert(knuth::base64::decode("TWE=") == "Ma"s);
-    assert(knuth::base64::decode("TWFu") == "Man"s);
+    assert(knuth::base64::encode("Man ") == "TWFuIA=="s);
     return 0;
 }
 #endif
