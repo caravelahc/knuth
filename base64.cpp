870189641e6f6d56c332e0f034ed2288fefbb500
Ranieri Althoff <ranisalt@gmail.com>
2018-08-25 23:15:10 -0300

    Implement base64 decode

--- base64.cpp
+++ base64.cpp
@@ -4,13 +4,54 @@
 
 namespace knuth::base64 {
 
-const char* alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                       "abcdefghijklmnopqrstuvwxyz"
-                       "0123456789+/";
+namespace {
 
-std::string decode(const std::string& s);
+const std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                             "abcdefghijklmnopqrstuvwxyz"
+                             "0123456789+/";
 
-std::string encode(const std::string& s) {
+char from_alphabet(char ch)
+{
+    if (ch >= 'A' and ch <= 'Z') {
+        return ch - 'A';
+    } else if (ch >= 'a' and ch <= 'z') {
+        return ch - 'a' + 26;
+    } else if (ch >= '0' and ch <= '9') {
+        return ch - '0' + 52;
+    } else if (ch == '+') {
+        return 62;
+    } else if (ch == '/') {
+        return 63;
+    }
+
+    throw std::runtime_error("Invalid input.");
+}
+
+}
+
+std::string decode(const std::string& s)
+{
+	std::string output;
+	output.reserve((s.size() / 4) * 3);
+
+    auto it = std::begin(s), end = std::end(s);
+    unsigned buffer = 0, buflen = 0;
+    while (it != end and *it != '=') {
+        buffer = (buffer << 6u) | from_alphabet(*it);
+        buflen += 6;
+
+        if (buflen >= 8) {
+            output.push_back(static_cast<char>(buffer >> (buflen - 8)));
+            buflen -= 8;
+        }
+        ++it;
+    }
+
+	return output;
+}
+
+std::string encode(const std::string& s)
+{
     std::string output;
     output.reserve(((s.size() + 2) / 3) * 4);
 
@@ -58,6 +99,10 @@ int main() {
     assert(knuth::base64::encode("Ma") == "TWE="s);
     assert(knuth::base64::encode("Man") == "TWFu"s);
     assert(knuth::base64::encode("Man ") == "TWFuIA=="s);
+    assert(knuth::base64::decode("TQ==") == "M"s);
+    assert(knuth::base64::decode("TWE=") == "Ma"s);
+    assert(knuth::base64::decode("TWFu") == "Man"s);
+    assert(knuth::base64::decode("TWFuIA==") == "Man "s);
     return 0;
 }
 #endif
